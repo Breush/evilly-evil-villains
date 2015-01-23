1cf545a44388a299b9c61b72081b385bfa30e9fd
S. Breust <sherlock.breust@gmail.com>
2015-01-23 16:49:26 +0100

    Fixing string2wstring on Windows

--- inc/core/gettext.hpp
+++ inc/core/gettext.hpp
@@ -1,22 +1,17 @@
 #pragma once
 
 #include <libintl.h>
-#include <locale.h>
 
+#include <locale>
 #include <string>
 #include <vector>
 
 inline std::wstring string2wstring(const std::string& s)
 {
-    // Conversion to char*, getting size
-    const char * cs = s.c_str();
-    const size_t wn = std::mbsrtowcs(nullptr, &cs, 0, nullptr);
+    std::wstring ws(s.size(), L' '); // Overestimate number of code points
+    ws.resize(mbstowcs(&ws[0], s.c_str(), s.size())); // Shrink to fit
 
-    // Buffering result
-    std::vector<wchar_t> buf(wn + 1);
-    std::mbsrtowcs(buf.data(), &cs, wn + 1, nullptr);
-
-    return std::wstring(buf.data(), wn);
+    return ws;
 }
 
 #define _(STRING) string2wstring(gettext(STRING))
