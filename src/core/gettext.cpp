c87f2d8c176cb2e013ff7e3051a073848408093c
S. Breust <sherlock.breust@gmail.com>
2015-03-01 18:12:55 +0100

    Protected non-Windows functions
    Make check does not work on Windows

--- src/core/gettext.cpp
+++ src/core/gettext.cpp
@@ -4,12 +4,15 @@
 
 void internationalization::init(const std::string& language)
 {
+    // MinGW does not know either setenv or putenv for some unknown reasons
+#if not defined(__WIN32__)
 	// Force a specific language
 	if (!language.empty()) {
 		setenv("LANG", language.c_str(), 1);
 		setenv("LANGUAGE", language.c_str(), 1);
 	}
-	
+#endif
+
     // General initialisation
     setlocale(LC_ALL, "");
 
