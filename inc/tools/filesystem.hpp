7547c91624e6aefa9175bda1d82d3e20974c1f38
S. Breust <sherlock.breust@gmail.com>
2015-03-09 14:04:33 +0100

    Made createDirectory work on Windows

--- inc/tools/filesystem.hpp
+++ inc/tools/filesystem.hpp
@@ -4,12 +4,22 @@
 
 #include <string>
 
-// POSIX
-#include <sys/stat.h>
-#include <sys/types.h>
+#if defined(__WIN32__)
+    // Windows
+    #include <windows.h>
+#else
+    // POSIX
+    #include <sys/stat.h>
+    #include <sys/types.h>
+#endif
 
 inline bool createDirectory(const std::wstring& dir)
 {
+#if defined(__WIN32__)
+    // Windows
+    return CreateDirectoryW(dir.c_str(), 0) != 0;
+#else
     // POSIX
     return mkdir(internationalization::wstring2string(dir).c_str(), S_IRWXU | S_IRWXG | S_IRWXO) == 0;
+#endif
 }
