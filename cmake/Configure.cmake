d4160abdfb9464bf4bdbdb9e3c4eca67ed9cd41e
S. Breust <sherlock.breust@gmail.com>
2015-04-05 21:01:01 +0200

    Fixed version extraction with SVN repository

--- cmake/Configure.cmake
+++ cmake/Configure.cmake
@@ -7,17 +7,22 @@
 set(VERSION_INPUT_FILE inc/core/version.hpp.in)
 set(VERSION_OUTPUT_FILE inc/core/version.hpp)
 
-# Check if repository is subversion or a git one.
-if (EXISTS ${CMAKE_SOURCE_DIR}/.svn)
-    execute_process(COMMAND svnversion .
-                    OUTPUT_VARIABLE EEV_VERSION_REVISION
-                    OUTPUT_STRIP_TRAILING_WHITESPACE)
-elseif (EXISTS ${CMAKE_SOURCE_DIR}/.git)
+# Check if repository is a git one.
+if (EXISTS ${CMAKE_SOURCE_DIR}/.git)
     execute_process(COMMAND git rev-list HEAD --count
                     OUTPUT_VARIABLE EEV_VERSION_REVISION
                     OUTPUT_STRIP_TRAILING_WHITESPACE)
-else()
-    # No revision information
+# Or maybe a svn one.
+else ()
+    execute_process(COMMAND svnversion .
+                    OUTPUT_VARIABLE EEV_VERSION_REVISION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+endif()
+
+# If it does not starts with a number, there is a problem.
+string(REGEX MATCH ^[0-9]+ EEV_VERSION_REVISION_EXTRACTED ${EEV_VERSION_REVISION})
+if (NOT EEV_VERSION_REVISION_EXTRACTED)
+    set(EEV_VERSION_REVISION_EXTRACTED NR)
     set(EEV_VERSION_REVISION NR)
 endif()
 
