531a1b130b49db9cd11a3705b88b005fb4275295
A. Breust <alexis.breust@gmail.com>
2015-04-05 19:39:19 +0200

    Enabled git to configure version

--- cmake/Configure.cmake
+++ cmake/Configure.cmake
@@ -7,7 +7,19 @@
 set(VERSION_INPUT_FILE inc/core/version.hpp.in)
 set(VERSION_OUTPUT_FILE inc/core/version.hpp)
 
-# TODO Check if subversion or git is available.
-execute_process(COMMAND svnversion -n . OUTPUT_VARIABLE EEV_VERSION_REVISION)
+# Check if repository is subversion or a git one.
+if (EXISTS ${CMAKE_SOURCE_DIR}/.svn)
+    execute_process(COMMAND svnversion .
+                    OUTPUT_VARIABLE EEV_VERSION_REVISION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+elseif (EXISTS ${CMAKE_SOURCE_DIR}/.git)
+    execute_process(COMMAND git rev-list HEAD --count
+                    OUTPUT_VARIABLE EEV_VERSION_REVISION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+else()
+    # No revision information
+    set(EEV_VERSION_REVISION NR)
+endif()
+
 configure_file(${CMAKE_SOURCE_DIR}/${VERSION_INPUT_FILE} ${CMAKE_SOURCE_DIR}/${VERSION_OUTPUT_FILE})
 
