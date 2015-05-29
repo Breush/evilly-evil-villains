c0801f8b43446ee62fb1943bbc65ab6b693be62b
S. Breust <sherlock.breust@gmail.com>
2015-05-29 09:08:13 +0200

    Added text selection to TextEntry

--- inc/nui/textentry.hpp
+++ inc/nui/textentry.hpp
@@ -8,58 +8,160 @@
 
 namespace nui
 {
+    //! An interactive entry allowing user to type text.
+
     class TextEntry final : public scene::Entity
     {
         using baseClass = scene::Entity;
 
     public:
-        TextEntry();
-        virtual ~TextEntry() = default;
 
-        // Routine
+        //! Constructor.
+        TextEntry();
+
+        //! Default destructor.
+        ~TextEntry() = default;
+
+        //--------------------------//
+        //! @name Getter and setter
+        //! @{
+
+        //! The current string.
+        std::wstring text() const { return m_textString.toWideString(); };
+
+        //! @}
+
+        //--------------------------//
+        //! @name Public properties
+        //! @{
+
+        // Size override, length is an estimated number of characters visible.
+        PARAMGSU(uint, m_length, length, setLength, updateSize)
+
+        // The max number of characters.
+        PARAMGS(uint, m_maxCharacters, maxCharacters, setMaxCharacters)
+
+        //! @}
+
+    protected:
+
+        //----------------//
+        //! @name Routine
+        //! @{
+
         void onSizeChanges() final;
-        void updateSize();
         void updateRoutine(const sf::Time& dt) final;
         void refreshDisplay() final;
 
-        // Getter
-        std::wstring text() const { return m_textString.toWideString(); };
+        //! Recompute the size of the TextEntry.
+        void updateSize();
 
-        // Size override, length is an estimated number of characters to draw.
-        // To limit the max number of characters, use setMaxCharacters().
-        PARAMGSU(uint, m_length, length, setLength, updateSize)
-        PARAMGS(uint, m_maxCharacters, maxCharacters, setMaxCharacters)
+        //! @}
 
-    protected:
-        // Events
-        bool handleKeyboardEvent(const sf::Event& event) override;
+        //---------------//
+        //! @name Events
+        //! @{
 
-        // Dynamics of text and cursor
+        void handleMouseButtonPressed(const sf::Mouse::Button button, const sf::Vector2f& mousePos, const sf::Vector2f& nuiPos) final;
+        void handleMouseButtonReleased(const sf::Mouse::Button button, const sf::Vector2f& mousePos, const sf::Vector2f& nuiPos) final;
+        void handleMouseMoved(const sf::Vector2f& mousePos, const sf::Vector2f& nuiPos) final;
+        bool handleKeyboardEvent(const sf::Event& event) final;
+
+        //! @}
+
+        //---------------//
+        //! @name Delete
+        //! @{
+
+        //! Remove the current selection.
+        bool deleteSelection();
+
+        //! Remove the previous word/character.
+        void deletePrevious();
+
+        //! Remove the next word/character.
+        void deleteNext();
+
+        //! @}
+
+        //------------------//
+        //! @name Selection
+        //! @{
+
+        //! Select the text between start and end.
+        //! The limits are not checked and should exists.
+        void select(uint start, uint end);
+
+        //! Cancel selection.
+        bool deselect();
+
+        //! @}
+
+        //---------------//
+        //! @name Cursor
+        //! @{
+
+        //! Returns the character position from relX, text will be filled with the string before this point.
+        uint getCharacterPosition(const float relX, sf::Text& text);
+
+        //! Add a character to the string where the cursor is.
+        void addCharacter(const uint32_t character);
+
+        //! Moves the cursor of some characters.
         void moveCursor(int relativePos);
-        void updateDynamicText();
+
+        //! Returns the start position of the previous word relative to cursor.
         int previousRelativeWord();
+
+        //! Returns the start position of the next word relative to cursor.
         int nextRelativeWord();
 
+        //! @}
+
+        //--------------------------------//
+        //! @name Internal change updates
+        //! @{
+
+        //! Refresh overlay of selection from m_selectString.
+        void refreshSelection();
+
+        //! Refresh overlay position of selection.
+        void refreshSelectionPosition();
+
+        //! Refresh the text position and move it if cursor is too far.
+        void updateDynamicText();
+
+        //! @}
+
     private:
-        // Background
-        sf::RectangleShape m_background;
+
+        sf::RectangleShape m_background;    //< The background.
 
         // Text
-        sf::String m_textString;
-        sf::Text m_text;
+        sf::String m_textString;        //!< The effective current string.
+        sf::Text m_text;                //!< The drawable text.
         float m_textShift = 0.f;        //!< Applied to follow the cursor when needed.
         float m_textWidthLimit = 0.f;   //!< The max visible width for the text.
 
         // Cursor
-        bool m_cursorOn = true;
-        sfe::VLine m_cursor;
-        float m_cursorBlinkTime = 0.f;
-        sf::String m_cursorString;
-        sf::Text m_cursorText;
+        bool m_cursorOn = true;         //!< Whether the cursor should blink.
+        sfe::VLine m_cursor;            //!< The drawable cursor.
+        float m_cursorBlinkTime = 0.f;  //!< How many seconds the cursor has been shown.
+        sf::String m_cursorString;      //!< The text before the cursor.
+        sf::Text m_cursorText;          //!< The text before the cursor.
+
+        // Selection
+        bool m_selecting = false;           //!< Whether the user has pressed the mouse button.
+        uint m_selectStart = 0u;            //!< The offset of start of selection.
+        sf::String m_selectString;          //!< The text selected.
+        sf::Text m_selectText;              //!< The text selected.
+        sf::RectangleShape m_selectRect;    //!< The overlay for selection
+        float m_selectShiftStart = 0.f;     //!< The shift from start of selection.
+        bool m_selectL2R = true;            //!< True if selection is left to right from start.
 
         // Decorum
-        float m_textPadding;
-        float m_fontVSpace;
-        float m_fontHSpace;
+        float m_textPadding = 0.f;  //!< The extra space between text and border.
+        float m_fontVSpace = 0.f;   //!< The estimated vertical space reserved for a character.
+        float m_fontHSpace = 0.f;   //!< The estimated horizontal space reserved for a character.
     };
 }
--- src/nui/textentry.cpp
+++ src/nui/textentry.cpp
@@ -3,10 +3,9 @@
 #include "core/application.hpp"
 #include "resources/identifiers.hpp"
 #include "config/nui.hpp"
+#include "tools/vector.hpp"
 
 using namespace nui;
-
-// TODO Add mouse selection of text
 
 TextEntry::TextEntry()
     : m_length(0u)
@@ -24,7 +23,12 @@ TextEntry::TextEntry()
     addPart(&m_cursor);
     m_cursor.setShade(0.f);
     m_cursor.setColor(sf::Color::Black);
+
+    addPart(&m_selectRect);
+    m_selectRect.setFillColor({100u, 60u, 40u, 60u});
+
     m_cursorText = m_text;
+    m_selectText = m_text;
 
     refreshDisplay();
 }
@@ -40,6 +44,7 @@ void TextEntry::onSizeChanges()
     m_textWidthLimit = size().x - 2.f * m_textPadding;
 
     setPartClippingRect(&m_text, {m_textPadding, 0.f, m_textWidthLimit, size().y});
+    setPartClippingRect(&m_selectRect, {m_textPadding, 0.f, m_textWidthLimit, size().y});
     updateDynamicText();
 }
 
@@ -68,6 +73,7 @@ void TextEntry::refreshDisplay()
 
     m_text.setCharacterSize(cNUI.fontSize);
     m_cursorText.setCharacterSize(cNUI.fontSize);
+    m_selectText.setCharacterSize(cNUI.fontSize);
 
     m_textPadding = (cNUI.hPadding + cNUI.vPadding) / 2.f;
     m_fontVSpace = cNUI.fontVSpace;
@@ -80,11 +86,43 @@ void TextEntry::refreshDisplay()
 //------------------//
 //----- Events -----//
 
+void TextEntry::handleMouseButtonPressed(const sf::Mouse::Button button, const sf::Vector2f& mousePos, const sf::Vector2f&)
+{
+    returnif (button != sf::Mouse::Left);
+
+    deselect();
+    m_selecting = true;
+
+    m_selectStart = getCharacterPosition(mousePos.x, m_cursorText);
+    m_selectShiftStart = boundsSize(m_cursorText).x;
+    m_cursorString = m_cursorText.getString();
+
+    updateDynamicText();
+}
+
+void TextEntry::handleMouseButtonReleased(const sf::Mouse::Button button, const sf::Vector2f& mousePos, const sf::Vector2f&)
+{
+    returnif (button != sf::Mouse::Left);
+    m_selecting = false;
+}
+
+void TextEntry::handleMouseMoved(const sf::Vector2f& mousePos, const sf::Vector2f&)
+{
+    returnif (!m_selecting);
+
+    uint selectEnd = getCharacterPosition(mousePos.x, m_cursorText);
+    m_cursorString = m_cursorText.getString();
+    select(m_selectStart, selectEnd);
+
+    updateDynamicText();
+}
+
 bool TextEntry::handleKeyboardEvent(const sf::Event& event)
 {
     // Left/right to move inside the text entry
     // Use Ctrl to skip a word
     if (event.type == sf::Event::KeyPressed) {
+        // TODO Add selection on shift + word selection on Ctrl
         if (event.key.code == sf::Keyboard::Left) {
             int toMove = sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)? previousRelativeWord() : -1;
             moveCursor(toMove);
@@ -98,52 +136,205 @@ bool TextEntry::handleKeyboardEvent(const sf::Event& event)
     }
     // Edit the text entry
     else if (event.type == sf::Event::TextEntered) {
-        // Enter
-        returnif (event.text.unicode == 13) false;
-
-        // Backspace
-        if (event.text.unicode == 8) {
-            returnif (m_cursorString.isEmpty()) true;
-            int toRemove = sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)? previousRelativeWord() : -1;
-            m_textString.erase(m_cursorString.getSize() + toRemove, -toRemove);
-            moveCursor(toRemove);
-        }
-        // Delete
-        else if (event.text.unicode == 127) {
-            returnif (m_cursorString == m_textString) true;
-            int toRemove = sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)? nextRelativeWord() : 1;
-            m_textString.erase(m_cursorString.getSize(), toRemove);
-        }
-        // Correct character
-        else {
-            returnif (m_textString.getSize() == m_maxCharacters) true;
-            m_textString = m_cursorString + event.text.unicode + m_textString.substring(m_cursorString.getSize());
-            moveCursor(1);
-        }
-
-        m_text.setString(m_textString);
-        updateDynamicText();
+        // TODO Associate a callback to Enter?
+        returnif (event.text.unicode == 13) false;          // Enter
+        if (event.text.unicode == 8) deletePrevious();      // Backspace
+        else if (event.text.unicode == 127) deleteNext();   // Delete
+        else addCharacter(event.text.unicode);              // Any correct character
         return true;
     }
 
     return false;
 }
 
-//----------------------//
-//----- Management -----//
+//------------------//
+//----- Delete -----//
+
+bool TextEntry::deleteSelection()
+{
+    returnif (m_selectString.isEmpty()) false;
+
+    if (m_selectL2R) {
+        m_cursorString = m_textString.substring(0u, m_selectStart);
+        m_textString.erase(m_selectStart, m_selectString.getSize());
+    }
+    else {
+        m_cursorString = m_textString.substring(0u, m_selectStart - m_selectString.getSize());
+        m_textString.erase(m_selectStart - m_selectString.getSize(), m_selectString.getSize());
+    }
+
+    m_cursorText.setString(m_cursorString);
+    m_text.setString(m_textString);
+
+    updateDynamicText();
+    deselect();
+    return true;
+}
+
+void TextEntry::deletePrevious()
+{
+    returnif (deleteSelection());
+    returnif (m_cursorString.isEmpty());
+
+    int toRemove = sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)? previousRelativeWord() : -1;
+    m_textString.erase(m_cursorString.getSize() + toRemove, -toRemove);
+    moveCursor(toRemove);
+
+    m_text.setString(m_textString);
+    updateDynamicText();
+}
+
+void TextEntry::deleteNext()
+{
+    returnif (deleteSelection());
+    returnif (m_cursorString == m_textString);
+
+    int toRemove = sf::Keyboard::isKeyPressed(sf::Keyboard::LControl)? nextRelativeWord() : 1;
+    m_textString.erase(m_cursorString.getSize(), toRemove);
+
+    m_text.setString(m_textString);
+    updateDynamicText();
+}
+
+//---------------------//
+//----- Selection -----//
+
+void TextEntry::select(uint start, uint end)
+{
+    m_selectL2R = (start < end);
+    if (m_selectL2R) m_selectString = m_textString.substring(start, end - start);
+    else  m_selectString = m_textString.substring(end, start - end);
+    refreshSelection();
+}
+
+bool TextEntry::deselect()
+{
+    returnif (m_selectString.isEmpty()) false;
+
+    m_selecting = false;
+    m_selectString.clear();
+    m_selectRect.setSize({0.f, 0.f});
+    refreshSelection();
+    return true;
+}
+
+//------------------//
+//----- Cursor -----//
+
+uint TextEntry::getCharacterPosition(const float relX, sf::Text& text)
+{
+    sf::String string;
+    float prevWidth = 0.f;
+    float correctX = relX + m_textShift - m_textPadding;
+
+    // Special case: click before the string
+    if (relX <= m_textPadding) {
+        text.setString(string);
+        return 0u;
+    }
+
+    for (uint i = 0u; i < m_textString.getSize(); ++i) {
+        string += m_textString[i];
+        text.setString(string);
+
+        // We just hit the next character,
+        // need to determine if we are closer to the previous one or not
+        float width = boundsSize(text).x;
+        if (width >= correctX) {
+            if (width - correctX > correctX - prevWidth) {
+                text.setString(string.substring(0u, string.getSize() - 1u));
+                return i;
+            }
+            else return i + 1u;
+        }
+
+        prevWidth = width;
+    }
+
+    return m_textString.getSize();
+}
+
+void TextEntry::addCharacter(const uint32_t character)
+{
+    deleteSelection();
+
+    returnif (m_textString.getSize() == m_maxCharacters);
+    m_textString = m_cursorString + character + m_textString.substring(m_cursorString.getSize());
+    moveCursor(1);
+
+    m_text.setString(m_textString);
+    updateDynamicText();
+}
 
 void TextEntry::moveCursor(int relativePos)
 {
+    bool shiftPressed = sf::Keyboard::isKeyPressed(sf::Keyboard::LShift);
+    returnif (!shiftPressed && deselect());
     returnif (relativePos == 0);
     returnif (relativePos < 0 && m_cursorString.isEmpty());
     returnif (relativePos > 0 && m_cursorString == m_textString);
 
+    // On shift, start selection if not already
+    if (shiftPressed && m_selectString.isEmpty()) {
+        m_selectStart = m_cursorString.getSize();
+        m_selectShiftStart = boundsSize(m_cursorText).x;
+    }
+
     auto cursorStringSize = m_cursorString.getSize() + relativePos;
     m_cursorString = m_textString;
-
     m_cursorString.erase(cursorStringSize, m_textString.getSize() - cursorStringSize);
     m_cursorText.setString(m_cursorString);
+
+    // On shift, also add to selection
+    if (shiftPressed) {
+        uint selectEnd = m_cursorString.getSize();
+        select(m_selectStart, selectEnd);
+    }
+
     updateDynamicText();
+}
+
+int TextEntry::previousRelativeWord()
+{
+    int cursorStringSize = m_cursorString.getSize() - 1;
+
+    int i = 0;
+    for (; i < cursorStringSize; ++i)
+        if (iswalnum(m_cursorString[cursorStringSize - i])) break;
+    for (; i < cursorStringSize; ++i)
+        returnif (!iswalnum(m_cursorString[cursorStringSize - i])) -i;
+
+    return -(cursorStringSize + 1);
+}
+
+int TextEntry::nextRelativeWord()
+{
+    int cursorStringSize = m_cursorString.getSize();
+    int textStringSize = m_textString.getSize() - 1;
+
+    int i = 0;
+    for (; i <= textStringSize - cursorStringSize; ++i)
+        if (!iswalnum(m_textString[cursorStringSize + i])) break;
+    for (; i <= textStringSize - cursorStringSize; ++i)
+        returnif (iswalnum(m_textString[cursorStringSize + i])) i;
+
+    return textStringSize - cursorStringSize + 1;
+}
+
+//-----------------------------------//
+//----- Internal change updates -----//
+
+void TextEntry::refreshSelection()
+{
+    m_selectText.setString(m_selectString);
+    m_selectRect.setSize({boundsSize(m_selectText).x, size().y - 2.f * m_textPadding});
+    refreshSelectionPosition();
+}
+
+void TextEntry::refreshSelectionPosition()
+{
+    if (m_selectL2R) m_selectRect.setPosition(m_textPadding + m_selectShiftStart - m_textShift, m_textPadding);
+    else m_selectRect.setPosition(m_textPadding - boundsSize(m_selectText).x + m_selectShiftStart - m_textShift, m_textPadding);
 }
 
 void TextEntry::updateDynamicText()
@@ -168,26 +359,9 @@ void TextEntry::updateDynamicText()
         m_textShift = 0.f;
     }
 
-    // Text
+    // Positions
     m_text.setPosition(m_textPadding - m_textShift, m_textPadding);
-
-    // Cursor
     m_cursor.setPosition({m_textPadding - m_textShift + cursorTextSize.x + 0.1f * m_textPadding, 0.5f * m_textPadding});
+    refreshSelectionPosition();
 }
 
-int TextEntry::previousRelativeWord()
-{
-    int cursorStringSize = m_cursorString.getSize() - 1;
-    for (int i = 0; i < cursorStringSize; ++i)
-        returnif (!iswalnum(m_cursorString[cursorStringSize - i])) -(i + 1);
-    return -(cursorStringSize + 1);
-}
-
-int TextEntry::nextRelativeWord()
-{
-    int cursorStringSize = m_cursorString.getSize();
-    int textStringSize = m_textString.getSize() - 1;
-    for (int i = 0; i <= textStringSize - cursorStringSize; ++i)
-        returnif (!iswalnum(m_textString[cursorStringSize + i])) i + 1;
-    return textStringSize - cursorStringSize + 1;
-}
