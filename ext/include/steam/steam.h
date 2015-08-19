a9153a361e98074ebe9b238522b69f7f1424cee6
A. Breust <alexis.breust@gmail.com>
2015-08-19 19:40:12 +0200

    Added Steam workshop interface

---
+++ ext/include/steam/steam.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// File created by A. Breust so that steam is in its own namespace.
+// Each time you need another function of the api, just include the file here.
+
+namespace steam
+{
+    #include "steam_api.h"
+    #include "steam_gameserver.h"
+}
+
--- src/core/main.cpp
+++ src/core/main.cpp
@@ -2,31 +2,48 @@
 #include "core/gettext.hpp"
 #include "core/debug.hpp"
 
+#include <steam/steam.h>
+
 #include <stdexcept>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
     Application app;
+    int returnStatus = EXIT_SUCCESS;
+
+    // Initialize language
     internationalization::init("");
 
     // Disable synchronisation
-    // Note: Remove the safety of using printf/scanf
-    // This makes the streams faster
+    // Note: Remove the safety of using printf/scanf (so don't use them)
+    // but this makes the streams faster.
     std::ios_base::sync_with_stdio(false);
 
     // Handle arguments
     for (int i = 1; i < argc; ++i)
-        mdebug_core_1("Argument: " << argv[i]);
+        mdebug_core_1("Argument " << i << " : " << argv[i]);
+
+    // Start Steam
+    bool steamActivated = false;
+    // No need to try: there nothing to activate unless we get an appID,
+    // which supposed to have been greenlit.
+    // steamActivated = steam::SteamAPI_Init();
+    mdebug_core_1("Steam interfacing: " << (steamActivated? "Enabled" : "Disabled"));
 
     // Running application
     try {
         app.run();
-    } catch(std::exception& e) {
+    }
+    catch(std::exception& e) {
         std::cerr << std::endl << "[!] Exception caught: " << e.what() << std::endl;
-        return EXIT_FAILURE;
+        returnStatus = EXIT_FAILURE;
     }
 
-    return EXIT_SUCCESS;
+    // Closing Steam
+    if (steamActivated)
+        steam::SteamAPI_Shutdown();
+
+    return returnStatus;
 }
 
