9fb0b2f892deccf1e40502806b4a3ec06dca9f01
A. Breust <alexis.breust@gmail.com>
2015-12-23 15:31:58 +0100

    Fixed lag spike when moving pickpock with piston

--- res/ai/dungeon/traps/pickpock.lua
+++ res/ai/dungeon/traps/pickpock.lua
@@ -9,6 +9,7 @@
 ------------
 -- Locals --
 
+local currentDosh       -- How much money this trap currently hold
 local maxDosh = 30      -- How much money this trap can hold
 local stealing = false  -- Whether the Pick-pock is already stealing
 
@@ -17,24 +18,23 @@ local stealing = false  -- Whether the Pick-pock is already stealing
 
 -- Called on new data
 function _reinit()
-    eev_initEmptyDataU32("dosh", 0)
+    currentDosh = eev_initEmptyDataU32("dosh", 0)
 end
 
 -- Called once on object creation
 function _register()
     eev_addCallback("cbHeroClose", "hero", "distance < 0.3")
-    
+
     eev_setDepth(60)
 end
 
 -- Whenever a hero comes too close
 function cbHeroClose(heroUID)
     if not stealing then
-        local dosh = eev_getDataU32("dosh")
         local heroDosh = eev_getUIDDataU32(heroUID, "dosh")
 
         -- Steal money if not full and hero has some on him
-        if dosh < maxDosh and heroDosh ~= 0 then
+        if currentDosh < maxDosh and heroDosh ~= 0 then
             -- Stop doing anything else
             stealing = true
             eev_selectAnimation("grab")
@@ -44,7 +44,8 @@ function cbHeroClose(heroUID)
             -- Steal
             local doshStolen = math.min(maxDosh - dosh, heroDosh)
             eev_setUIDDataU32(heroUID, "dosh", heroDosh - doshStolen)
-            eev_setDataU32("dosh", doshStolen)
+            currentDosh = currentDosh + doshStolen
+            eev_setDataU32("dosh", currentDosh)
             eev_warnHarvestableDosh();
         end
     end
@@ -66,13 +67,12 @@ end
 
 -- Called for harvesting money
 function _harvestDosh()
-    local dosh = eev_getDataU32("dosh")
-    eev_setDataU32("dosh", 0)
+    local dosh = currentDosh
+    currentDosh = eev_setDataU32("dosh", 0)
     return dosh
 end
 
 -- Called for finding out how muh money the trap holds
 function _harvestableDosh()
-    local dosh = eev_getDataU32("dosh")
-    return dosh
+    return currentDosh
 end
